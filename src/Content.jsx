643e88841ffe04fe05a307cbc7620f260f65f872
Aleksandr Masko <amasko18@gmail.com>
2022-02-25 23:33:53 +0300

    commit

--- src/Content.jsx
+++ src/Content.jsx
@@ -2,7 +2,24 @@ const Content = () => {
     return(
         <div className="content">
             <img src ="https://cdn.motor1.com/images/mgl/zZ9Wq/s1/2020-audi-rs6-avant.jpg" className="img__content"></img>
+            <div>ava+description</div>
+      <div>My posts
+        <div>
+          New Posts
         </div>
+        <div>
+          <div>
+            Post 1
+          </div>
+          <div>
+            Post 2
+          </div>
+          <div>
+            Post 3
+          </div>
+        </div>
+      </div>
+    </div>
     )
 }
 
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,9 @@
 const Header = () => {
     return (
         <div className="header">
+            <div className="logo">
+                <img src="../img/audiLogo.png"></img>
+            </div>
             <a href = '#home'>Home</a>
             <a href = '#News'>News Feed</a>
             <a href = '#Messages'>Messages</a>
