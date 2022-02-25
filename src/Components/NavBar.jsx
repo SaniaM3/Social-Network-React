a43728316a2caffb900b2e16488030c205060a32
Aleksandr Masko <amasko18@gmail.com>
2022-02-25 23:48:47 +0300

    commit

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,19 +1,19 @@
 const NavBar = () => {
     return(
         <div className="nav">
-            <div>
+            <div className="item">
                 <a href = '#1'>Profile</a>
             </div>
-            <div>
+            <div className="item">
                 <a href = '#2'>Messages</a>
             </div>
-            <div>
+            <div className="item">
                 <a href = '#3'>News</a>
             </div>
-            <div>
+            <div className="item">
                 <a href = '#3'>Music</a>
             </div>
-            <div>
+            <div className="item">
                 <a href = '#3'>Settings</a>
             </div>
         </div>
