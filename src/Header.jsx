af55ec136da3ef4790c2841fe8aacd37094d91f1
Aleksandr Masko <amasko18@gmail.com>
2022-02-25 20:15:50 +0300

    commit

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <div>
+            <a href = '#home'>Home</a>
+            <a href = '#home'>News Feed</a>
+            <a href = '#home'>Messages</a>
+        </div>
+    )
+    }
+
+    export default Header;
