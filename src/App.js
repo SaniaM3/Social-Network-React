c883ddbd7e1786dca6761f52ca718ccf46c1eb39
Aleksandr Masko <amasko18@gmail.com>
2022-02-26 09:36:53 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import Header from './Components/Header';
-import Profile from './Components/Profile';
-import NavBar from './Components/NavBar';
+import Header from './Components/Header/Header';
+import Profile from './Components/Profile/Profile';
+import NavBar from './Components/NavBar/NavBar';
 
 function App() {
   return (
