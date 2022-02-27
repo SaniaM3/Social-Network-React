1261c2a633075c37e91df5ee70d816f1e672f22f
Aleksandr Masko <amasko18@gmail.com>
2022-02-27 12:58:51 +0300

    commit

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,8 +1,34 @@
 
+import s from './Dialogs.module.css'
+
 const Dialogs = (props) => {
     return(
-        <div>
-        Dialogs
+        <div className={s.dialogs}>
+        <div className={s.dialogsItems}>
+            <div className={s.dialog}>
+                Andrey
+            </div>
+            <div className={s.dialog}>
+                Sasha
+            </div>
+            <div className={s.dialog}>
+                Masha
+            </div>
+            <div className={s.dialog}>
+                Petya
+            </div>
+        </div>
+        <div className={s.messages}>
+            <div className={s.message}>
+                Hi
+            </div>
+            <div className={s.message}>
+                Hi are you?
+            </div>
+            <div className={s.message}>
+                Fine, Fuck!!!
+            </div>
+        </div>
         </div>
     )
 }
