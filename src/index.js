18dbd57a6b8ad274882adc4665ffe8bce622e8cc
Aleksandr Masko <amasko18@gmail.com>
2022-03-01 12:36:51 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -5,25 +5,25 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
-let postData = [
-  {id: 1, message: 'Hi', likesCounte: 16},
-  {id: 2, message: 'Hi, how are you?', likesCounte: 39},
-  {id: 3, message: 'Oh, yeaa', likesCounte: 82},
-  {id: 4, message: 'Hello', likesCounte: 11},
-]
+// let postData = [
+//   {id: 1, message: 'Hi', likesCounte: 16},
+//   {id: 2, message: 'Hi, how are you?', likesCounte: 39},
+//   {id: 3, message: 'Oh, yeaa', likesCounte: 82},
+//   {id: 4, message: 'Hello', likesCounte: 11},
+// ]
 
-let dialogs = [
-  {id: 1, name: 'Dimych'},
-  {id: 2, name: 'Sasha'},
-  {id: 3, name: 'Masha'},
-  {id: 4, name: 'Petya'},
-]
+// let dialogs = [
+//   {id: 1, name: 'Dimych'},
+//   {id: 2, name: 'Sasha'},
+//   {id: 3, name: 'Masha'},
+//   {id: 4, name: 'Petya'},
+// ]
 
-let messages = [
-  {id: 1, message: 'Hi'},
-  {id: 2, message: 'Hi are you?'},
-  {id: 3, message: 'Fine, Fuck!!!'},
-]
+// let messages = [
+//   {id: 1, message: 'Hi'},
+//   {id: 2, message: 'Hi are you?'},
+//   {id: 3, message: 'Fine, Fuck!!!'},
+// ]
 
 
 ReactDOM.render(
@@ -32,7 +32,7 @@ ReactDOM.render(
   <React.StrictMode>
     
     <BrowserRouter>
-    <App dialogs = {dialogs} messages = {messages} postData = {postData}/>
+    <App />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
