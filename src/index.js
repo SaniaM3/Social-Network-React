a845c4de22af262d02507feb1c086c6b17a558da
Aleksandr Masko <amasko18@gmail.com>
2022-03-01 12:58:09 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -6,32 +6,6 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import state from './redux/state';
 
-// let postData = [
-//   {id: 1, message: 'Hi', likesCounte: 16},
-//   {id: 2, message: 'Hi, how are you?', likesCounte: 39},
-//   {id: 3, message: 'Oh, yeaa', likesCounte: 82},
-//   {id: 4, message: 'Hello', likesCounte: 11},
-// ]
-
-// let dialogs = [
-//   {id: 1, name: 'Dimych'},
-//   {id: 2, name: 'Sasha'},
-//   {id: 3, name: 'Masha'},
-//   {id: 4, name: 'Petya'},
-// ]
-
-// let messages = [
-//   {id: 1, message: 'Hi'},
-//   {id: 2, message: 'Hi are you?'},
-//   {id: 3, message: 'Fine, Fuck!!!'},
-// ]
-// let postData = [
-//   {id: 1, message: 'Hi', likesCounte: 16},
-//   {id: 2, message: 'Hi, how are you?', likesCounte: 39},
-//   {id: 3, message: 'Oh, yeaa', likesCounte: 82},
-//   {id: 4, message: 'Hello', likesCounte: 11},
-// ]
-
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
