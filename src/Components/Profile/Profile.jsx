8811756e4cc0bfb2cf9b3c71e70fb49357498cd8
Aleksandr Masko <amasko18@gmail.com>
2022-03-01 12:37:00 +0300

    commit

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -5,10 +5,17 @@ import ProfileInfo from './ProfileInfo/ProfileInfo';
 
 const Profile = (props) => {
 
+  let postData = [
+    {id: 1, message: 'Hi', likesCounte: 16},
+    {id: 2, message: 'Hi, how are you?', likesCounte: 39},
+    {id: 3, message: 'Oh, yeaa', likesCounte: 82},
+    {id: 4, message: 'Hello', likesCounte: 11},
+  ]
+  
     return(
         <div>
           <ProfileInfo />
-          <MyPosts postData = {props.postData}/>
+          <MyPosts postData = {postData}/>
         </div>
         
     )
