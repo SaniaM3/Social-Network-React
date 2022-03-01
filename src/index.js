449e01e3d9204e64d6111d335f5b9fb6c2bd4986
Aleksandr Masko <amasko18@gmail.com>
2022-03-01 10:32:51 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -5,14 +5,39 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
+let postData = [
+  {id: 1, message: 'Hi', likesCounte: 16},
+  {id: 2, message: 'Hi, how are you?', likesCounte: 39},
+  {id: 3, message: 'Oh, yeaa', likesCounte: 82},
+  {id: 4, message: 'Hello', likesCounte: 11},
+]
+
+let dialogs = [
+  {id: 1, name: 'Dimych'},
+  {id: 2, name: 'Sasha'},
+  {id: 3, name: 'Masha'},
+  {id: 4, name: 'Petya'},
+]
+
+let messages = [
+  {id: 1, message: 'Hi'},
+  {id: 2, message: 'Hi are you?'},
+  {id: 3, message: 'Fine, Fuck!!!'},
+]
+
+
 ReactDOM.render(
+
+  
   <React.StrictMode>
+    
     <BrowserRouter>
-    <App />
+    <App dialogs = {dialogs} messages = {messages} postData = {postData}/>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
