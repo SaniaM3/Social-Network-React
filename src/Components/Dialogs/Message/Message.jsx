3f31ca796c54a0845ae78fd8488fa58a0b119870
Aleksandr Masko <amasko18@gmail.com>
2022-03-02 21:00:47 +0300

    commit

--- src/Components/Dialogs/Message/Message.jsx
+++ src/Components/Dialogs/Message/Message.jsx
@@ -5,7 +5,7 @@ import s from './../Dialogs.module.css'
 const Message = (props) => {
     return(
         <div className={s.message}>
-                {props.message}
+                {props.message} 
             </div>
     )
 }
