3f285b9f75371af834b27a34f2e0b7bdfb05d1e8
Aleksandr Masko <amasko18@gmail.com>
2022-03-03 12:04:25 +0300

    commit

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,4 +1,5 @@
 
+import React from 'react';
 import DialogItem from './DialogItem/Dialogs';
 import s from './Dialogs.module.css'
 import Message from './Message/Message';
@@ -10,6 +11,12 @@ const Dialogs = (props) => {
     let dialogsElements = props.dialogs.map( d => <DialogItem name = {d.name} id = {d.id}/>)
 
     let messagesElements = props.messages.map( m => <Message message = {m.message}/>)
+
+    let newMessage = React.createRef();
+    let addMessage = () => {
+        let messageText = newMessage.current.value;
+        alert (messageText);
+    }
     
     return(
         <div className={s.dialogs}>
@@ -18,8 +25,14 @@ const Dialogs = (props) => {
             </div>
             <div className={s.messages}>
                 {messagesElements}
+                <div>
+                    <textarea ref = {newMessage} className={s.area_message} cols="30" rows="5"></textarea>
+                </div>
+                <button onClick={addMessage}>Отправить</button>
             </div>
+            
         </div>
+        
     )
 }
 
