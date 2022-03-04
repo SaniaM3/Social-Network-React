04825b2ebf857e7f12ab3214708916462e13cc6e
Aleksandr Masko <amasko18@gmail.com>
2022-03-04 10:57:45 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,44 @@
 
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { rerenderEntireTree } from './render';
-import state from './redux/state';
+import state, { subscribe } from './redux/state';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { updateNewPostText } from './redux/state';
+import {addPost} from './redux/state';
 
 
+let rerenderEntireTree = (state) => {
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <App postData = {state.profilePage.postData} 
+    dialogs = {state.dialogsPage.dialogs}
+    messages = {state.dialogsPage.messages}
+    addPost = {addPost}
+    updateNewPostText = {updateNewPostText}/>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+}
 rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree);
+
+
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
