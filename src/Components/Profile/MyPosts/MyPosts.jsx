6a61a6cdc1c07115c4a16e36f8552412203c1b82
Aleksandr Masko <amasko18@gmail.com>
2022-03-04 10:32:17 +0300

    commit

--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -9,19 +9,20 @@ const MyPosts = (props) => {
     let newPostElement = React.createRef();
 
     let addPost = () => {
-      let text = newPostElement.current.value;
-      props.addPost(text);
-      newPostElement.current.value = {};
+      props.addPost();
     }
 
-
+    let onPostChange = () => {
+      let text = newPostElement.current.value;
+      props.updateNewPostText(text);
+    }
     return(
       <div className={s.postsBlock}>
         <div>
           New Posts
         </div>
         <div>
-          <textarea ref={newPostElement} value = ''/>
+          <textarea ref={newPostElement} onChange = {onPostChange} value = {props.newPostText}/>
         </div>
         <div>
           <button onClick={ addPost }>Add Post</button>
