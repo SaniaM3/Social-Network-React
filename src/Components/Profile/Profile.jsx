9bb16067306d154dbda2f2dbe85d4a6ab037c839
Aleksandr Masko <amasko18@gmail.com>
2022-03-04 10:32:09 +0300

    commit

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -7,7 +7,11 @@ const Profile = (props) => {
     return(
         <div>
           <ProfileInfo />
-          <MyPosts postData = {props.postData} addPost = {props.addPost}/>
+          <MyPosts 
+            postData = {props.postData} 
+            addPost = {props.addPost} 
+            newPostText =  {props.newPostText}
+            updateNewPostText = {props.updateNewPostText}/>
         </div>
         
     )
