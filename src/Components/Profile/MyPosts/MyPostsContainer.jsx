b7c0554301917937e0a70b420b060d711e5f9ac0
Aleksandr Masko <amasko18@gmail.com>
2022-03-17 23:52:15 +0300

    commit

---
+++ src/Components/Profile/MyPosts/MyPostsContainer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { addPostActionCreator, updateNewPostTextActionCreator } from '../../../redux/profile-reducer';
+import MyPosts from './MyPosts';
+
+
+
+const MyPostsContainer = (props) => {
+
+    let addPost = () => {
+      props.dispatch(addPostActionCreator());
+    }
+
+    let onPostChange = (text) => {
+      let action = updateNewPostTextActionCreator(text)
+      props.dispatch(action);
+    }
+    return(
+      <MyPosts onPostChange = {onPostChange} addPost = {addPost} />
+    )
+}
+
+export default MyPostsContainer;
+.
