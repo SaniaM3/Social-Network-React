624e58a2e065f692617bb806ad15252917513ba7
Aleksandr Masko <amasko18@gmail.com>
2022-03-28 20:45:01 +0300

    commit

--- src/Components/Profile/MyPosts/Post/Post.jsx
+++ src/Components/Profile/MyPosts/Post/Post.jsx
@@ -4,8 +4,8 @@ import s from './Post.module.css'
 const Post = (props) => {
     return(
         <div className={s.item}>
-          <img src="https://www.publicdomainpictures.net/pictures/270000/nahled/avatar-people-person-business-u.jpg" className={s.avatar}></img>
-          <a href = "#">{props.message}</a>
+          <img src="https://mpng.subpng.com/20180411/rzw/kisspng-user-profile-computer-icons-user-interface-mystique-5aceb0245aa097.2885333015234949483712.jpg" className={s.avatar}></img>
+          <a href = "">{props.message}</a>
           <div>
           <span>Likes {props.like}</span>
           </div>
